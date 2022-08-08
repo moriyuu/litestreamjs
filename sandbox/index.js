62dbe546c63ca75ad30893f3962ead5e28f6454f
Yuta Mori <moriyu54321@gmail.com>
2022-08-09 06:08:16 +0900

    init

---
+++ sandbox/index.js
@@ -0,0 +1,3 @@
+const litestreamjs = require("../dist").default;
+
+litestreamjs();
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { platform } from "os";
+
+const main = async () => {
+  console.log("os.platform() :>> ", platform());
+};
+
+export default main;
